3264518e09d6059892c9af3950884af2d929c1a4
wilson <wilson@gdn-commerce.com>
2021-03-21 00:28:34 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const navToogle = document.querySelector('.nav-toggle');
+const navLinks = document.querySelectorAll('.nav__link');
+
+navToogle.addEventListener('click', () => {
+    document.body.classList.toggle('nav-open');
+})
+
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        document.body.classList.remove('nav-open');
+    })
+})
