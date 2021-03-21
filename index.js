ddbb17aa8838413398908cf9733cc9c439ba16ea
wilson <wilson@gdn-commerce.com>
2021-03-21 10:33:56 +0700

    format

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const navToogle = document.querySelector('.nav-toggle');
-const navLinks = document.querySelectorAll('.nav__link');
+const navToogle = document.querySelector(".nav-toggle");
+const navLinks = document.querySelectorAll(".nav__link");
 
-navToogle.addEventListener('click', () => {
-    document.body.classList.toggle('nav-open');
-})
+navToogle.addEventListener("click", () => {
+    document.body.classList.toggle("nav-open");
+});
 
-navLinks.forEach(link => {
-    link.addEventListener('click', () => {
-        document.body.classList.remove('nav-open');
-    })
-})
+navLinks.forEach((link) => {
+    link.addEventListener("click", () => {
+        document.body.classList.remove("nav-open");
+    });
+});
